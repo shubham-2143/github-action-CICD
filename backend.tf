e41c1223ffc51816315fafaf03bffba68b751fe5
shubham-2143 <shubhamsontakke0062@gmail.com>
2026-02-02 13:40:35 +0530

    Add Terraform backend configuration for S3

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket = "shubhamsontakke"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    #use_lockfile = true tarraform version should be above 1.10
+    #dynamodb_table = "abcd"
+  }
+}
